7377ce0d2b203e2a579dfdeef9c7fdbec92fb7c7
Zeka Shamilli <zekashamilli17@gmail.com>
2026-04-02 11:22:58 -0700

    fix: re-add registration_end_date to activity update (column exists in DB)

--- api/index.js
+++ api/index.js
@@ -2282,6 +2282,12 @@ module.exports = async function handler(req, res) {
           paramCount++;
         }
         
+        if (registrationEndDate !== undefined) {
+          updates.push(`registration_end_date = $${paramCount}`);
+          values.push(registrationEndDate);
+          paramCount++;
+        }
+
         updates.push(`updated_at = NOW()`);
         values.push(activityId);
         
